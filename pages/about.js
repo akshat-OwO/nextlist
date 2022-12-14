b2b94c1d3753230deff3100f9976354618f024a0
Akshat Singh <akshatsinghdelhi@gmail.com>
2022-12-14 18:00:40 +0530

    Pages & Routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
+        </div>
+    );
+}
+ 
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
     </div>
   )
 }
