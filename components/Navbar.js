9fdc183533a24ab7ccf02aa2d5a8624302e63e53
Akshat Singh <akshatsinghdelhi@gmail.com>
2022-12-14 18:26:35 +0530

    Components

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>Home</h2>
+            <h2>About</h2>
+            <h2>Ninja Listing</h2>
+        </nav>
+    );
+}
+ 
+export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+      <Navbar />
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
     </div>
