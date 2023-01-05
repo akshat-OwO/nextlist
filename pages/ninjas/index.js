22920ee1f15a95ed5fd69f4fd7b15b40d0c01f6a
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-05 19:52:09 +0530

    Dynamic Routes

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../../styles/Ninjas.module.css';
 
 export const getStaticProps = async () => {
@@ -16,9 +17,9 @@ const Ninjas = ({ ninjas }) => {
             <h1>All Ninjas</h1>
             {ninjas.map(ninja => (
                 <div key={ninja.id}>
-                    <a className={styles.single}>
+                    <Link href={'/ninjas/' + ninja.id} className={styles.single}>
                         <h3>{ ninja.name }</h3>
-                    </a>
+                    </Link>
                 </div>
             ))}
         </div>
