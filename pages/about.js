2cec0fe68a88b9b0ffc4d3e01f8b9fe3f8929170
Akshat Singh <akshatsinghdelhi@gmail.com>
2023-01-05 18:40:37 +0530

    Images & Metadata

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,17 @@
+import Head from "next/head";
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
-        </div>
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quisquam mollitia quibusdam laudantium reiciendis commodi magni dolorum vel deleniti perspiciatis!</p>
+            </div>
+        </>
     );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,18 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      <h2 className={styles.title}>Homepage</h2>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h2 className={styles.title}>Homepage</h2>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero incidunt, commodi qui nihil tempore aliquid error explicabo. Maxime, quidem eius!</p>
 
-      <Link className={styles.btn} href="/ninjas">See Ninja Listing</Link>
-    </div>
+        <Link className={styles.btn} href="/ninjas">See Ninja Listing</Link>
+      </div>
+    </>
   )
 }
